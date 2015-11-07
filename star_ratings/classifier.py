0f7111ced100845e7d10589d153c90288dd69de0
Jonathan <tiaojon@umich.edu>
2015-11-07 14:03:16 -0500

    parse business data

--- star_ratings/classifier.py
+++ star_ratings/classifier.py
@@ -1,28 +1,43 @@
-import featurizer
-import gen_training_data
-import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import SGDClassifier
+def classify_reviews():
+	import featurizer
+	import gen_training_data
+	import numpy as np
+	from sklearn.naive_bayes import MultinomialNB
+	from sklearn.linear_model import SGDClassifier
 
-data = gen_training_data.gen_training_data();
-stemmed_data = featurizer.stem(data);
-tfidf= featurizer.tfidf(data);
-clf = MultinomialNB().fit(tfidf['train_tfidf'], data['training_labels']);
-predicted = clf.predict(tfidf['test_tfidf']);
-num_wrong = 0;
-tot = 0;
-for expected, guessed in zip(data['testing_labels'], predicted):
-	if(expected-guessed != 0):	
-		num_wrong += 1;
+	data = gen_training_data.gen_data();
+	stemmed_data = featurizer.stem(data);
+	tfidf= featurizer.tfidf(data);
+	clf = MultinomialNB().fit(tfidf['train_tfidf'], data['training_labels']);
+	predicted = clf.predict(tfidf['test_tfidf']);
+	num_wrong = 0;
+	tot = 0;
+	for expected, guessed in zip(data['testing_labels'], predicted):
+		if(expected-guessed != 0):	
+			num_wrong += 1;
 
-print("num_wrong: %d",num_wrong)
+	print("num_wrong: %d",num_wrong)
 
-sgd_clf = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42);
-_ = sgd_clf.fit(tfidf['train_tfidf'], data['training_labels']);
-sgd_pred = sgd_clf.predict(tfidf['test_tfidf']);
-print np.mean(sgd_pred == data['testing_labels']);
+	sgd_clf = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42);
+	_ = sgd_clf.fit(tfidf['train_tfidf'], data['training_labels']);
+	sgd_pred = sgd_clf.predict(tfidf['test_tfidf']);
+	print np.mean(sgd_pred == data['testing_labels']);
 
-stem_tfidf = featurizer.tfidf(stemmed_data);
-_ = sgd_clf.fit(stem_tfidf['train_tfidf'], data['training_labels']);
-sgd_stem_prd = sgd_clf.predict(stem_tfidf['test_tfidf']);
-print np.mean(sgd_stem_prd==data['testing_labels']);
+	stem_tfidf = featurizer.tfidf(stemmed_data);
+	_ = sgd_clf.fit(stem_tfidf['train_tfidf'], data['training_labels']);
+	sgd_stem_prd = sgd_clf.predict(stem_tfidf['test_tfidf']);
+	print np.mean(sgd_stem_prd==data['testing_labels']);
+
+def classify_user_reviews():
+	import featurizer
+	import gen_training_data
+	import numpy as np
+	from sklearn.naive_bayes import MultinomailNB
+	from sklearn.linear_model import SGDClassifier
+
+	data = gen_training_data.gen_user_data();
+	user_classifiers = {};
+	
+
+if __name__ == "__main__":
+	classify_reviews();
--- star_ratings/gen_training_data.py
+++ star_ratings/gen_training_data.py
@@ -1,4 +1,4 @@
-def gen_training_data():
+def gen_data():
 	import json
 	training = []
 	training_labels = [] 
@@ -8,15 +8,82 @@ def gen_training_data():
 		i = 0;
 		for review in yelp_reviews:
 			review_contents = json.loads(review);
-			if(i > 10000):
+			if(i > 9000):
 				testing.append(review_contents['text']);
 				testing_labels.append(review_contents['stars'] );	
 			else:
 				training.append(review_contents['text']);
 				training_labels.append(review_contents['stars'] );
-			if(i > 10100):
-				break;
 			i = i+1;
 	
 	
 	return {'training':training, 'training_labels':training_labels, 'testing':testing, 'testing_labels':testing_labels}
+
+def parse_business_data():
+	import json
+	import copy
+	from scipy import sparse
+	import cPickle as pickle
+	categories = {};
+	business_categories = {};
+	with open('../../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json') as yelp_businesses:
+			for business in yelp_businesses:
+				business_contents = json.loads(business);
+				for category in business_contents['categories']:
+					categories[category] = 0;
+	
+	with open('../../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json') as yelp_businesses:
+			for business in yelp_businesses:
+				business_contents = json.loads(business);
+				business_categories_list = copy.deepcopy(categories);
+				for category in business_contents['categories']:
+					business_categories_list[category] = 1;
+				business_categories[business_contents['business_id']] = sparse.csr_matrix(business_categories_list.values());
+	
+	with open('business_data.p', 'wb') as fp:
+		pickle.dump(business_categories, fp);
+	return business_categories;	
+
+
+def cluster_users():
+	import json
+	training = []
+	training_labels = [] 
+	testing_category = {}
+	
+	testing = []
+	testing_labels = []
+	with open('../../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json') as yelp_reviews:
+		i = 0;
+		for review in yelp_reviews:
+			review_contents = json.loads(review);
+			if(i > 9000):
+				if(review_contents['user_id'] in testing):
+					testing[review_contents['user_id']].append(review_contents['text']);
+					testing_labels[review_contents['user_id']].append(review_contents['stars']);
+					testing_category[review_contents['user_id']].append(review_contents['categories'])
+				else:
+					testing[review_contents['user_id']] = []
+					testing_labels[review_contents['user_id']] = []
+#testing_category[review_contents['user_id']] = []
+					testing[review_contents['user_id']].append(review_contents['user_id']);
+					testing_labels[review_contents['user_id']].append(review_contents['user_id']);
+#					testing_category[review_contents['user_id']].append(review_contents['user_id']);
+			else:
+				if(review_contents['user_id'] in training):
+					training[review_contents['user_id']].append(review_contents['text']);
+					training_labels[review_contents['user_id']].append(review_contents['stars']);
+#				training_category[review_contents['user_id']].append(review_contents['categories']);
+				else:
+					training[review_contents['user_id']] = []
+					training_labels[review_contents['user_id']] = []
+#training_category[review_contents['user_id']] = []
+					training[review_contents['user_id']].append(review_contents['user_id']);
+					training_labels[review_contents['user_id']].append(review_contents['user_id']);
+#					training_category[review_contents['user_id']].append(review_contents['categories']);
+			i = i+1;	
+	
+#	return {'training':training, 'training_labels':training_labels, 'testing':testing, 'testing_labels':testing_labels, 'training_categories' : training_category, 'testing_categories':testing_category}
+	
+if __name__ == '__main__':
+	parse_business_data();
